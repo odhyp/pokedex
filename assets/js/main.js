490d469398bc42673acf278851257897c3196dd9
odhyp <pradhana.odhy@gmail.com>
2024-07-22 14:27:14 +0700

    feat: add auto margin top function

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,18 @@ function toggleHeaderShadow() {
 }
 
 window.addEventListener("scroll", toggleHeaderShadow);
+
+// Give main a margin-top to account for navbar height
+document.addEventListener('DOMContentLoaded', function() {
+  var navbar = document.getElementById('my-header');
+  var main = document.getElementById('main-section');
+
+  function adjustMarginTop() {
+    var navbarHeight = navbar.offsetHeight;
+    main.style.marginTop = (navbarHeight * 2) + 'px';
+  }
+
+  adjustMarginTop();
+
+  window.addEventListener('resize', adjustMarginTop);
+});
