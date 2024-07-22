80eb0f425710034c8c26517905cb779650b643e2
odhyp <pradhana.odhy@gmail.com>
2024-07-22 13:24:41 +0700

    feat: add species data fetcher

--- main.py
+++ main.py
@@ -11,3 +11,12 @@ def fetch_pokemon_data(pokemon_id):
         print(f"Failed to fetch data for Pokémon ID: {pokemon_id}")
         return None
 
+
+def fetch_pokemon_species_data(pokemon_id):
+    url = f'https://pokeapi.co/api/v2/pokemon-species/{pokemon_id}/'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Failed to fetch species data for Pokémon ID: {pokemon_id}")
+        return None
