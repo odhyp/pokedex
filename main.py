c1d243011725eb408c174eb8aaa59e592ba2075b
odhyp <pradhana.odhy@gmail.com>
2024-07-22 14:34:24 +0700

    test: add sample test function

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from src.data_fetcher import DataFetcher
 def main():
     data_fetcher = DataFetcher()
     sample = data_fetcher.fetch_pokemon_data(1)
-    print(sample)
+    print(sample["name"])
+
+    sample2 = data_fetcher.fetch_pokemon_species_data(1)
+    print(sample2["is_baby"])
 
 
 if __name__ == '__main__':
