7936e397e0d2013cf7c4af652d52602334d3e3e7
odhyp <pradhana.odhy@gmail.com>
2024-07-22 14:31:45 +0700

    feat: move fetcher to data_fetcher.py

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
-import requests
-import os
+from src.data_fetcher import DataFetcher
 
 
-def fetch_pokemon_data(pokemon_id):
-    url = f'https://pokeapi.co/api/v2/pokemon/{pokemon_id}/'
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print(f"Failed to fetch data for Pokémon ID: {pokemon_id}")
-        return None
+def main():
+    data_fetcher = DataFetcher()
+    sample = data_fetcher.fetch_pokemon_data(1)
+    print(sample)
 
 
-def fetch_pokemon_species_data(pokemon_id):
-    url = f'https://pokeapi.co/api/v2/pokemon-species/{pokemon_id}/'
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        print(f"Failed to fetch species data for Pokémon ID: {pokemon_id}")
-        return None
+if __name__ == '__main__':
+    main()
---
+++ src/data_fetcher.py
@@ -0,0 +1,22 @@
+import requests
+import os
+
+
+class DataFetcher:
+    def fetch_pokemon_data(self, pokemon_id):
+        url = f'https://pokeapi.co/api/v2/pokemon/{pokemon_id}/'
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Failed to fetch data for Pokémon ID: {pokemon_id}")
+            return None
+
+    def fetch_pokemon_species_data(self, pokemon_id):
+        url = f'https://pokeapi.co/api/v2/pokemon-species/{pokemon_id}/'
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print(f"Failed to fetch species data for Pokémon ID: {pokemon_id}")
+            return None
