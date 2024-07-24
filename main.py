2fea8aa7944feb689f2c9e63163c0f9428748c3c
odhyp <pradhana.odhy@gmail.com>
2024-07-24 23:29:01 +0700

    test: add test run functions

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
+import os
+import shutil
+
 from src.data_fetcher import DataFetcher
+from src.generate_content import GenerateContent
+
+
+NUMBER_OF_POKEMON = 6
+
+
+def load_data(number_of_pokemon):
+    for i in range(number_of_pokemon):
+        i += 1
+
+        # Fetching base data and species data
+        data_fetcher = DataFetcher()
+        base_data = data_fetcher.fetch_pokemon_data(pokemon_id=i)
+        species_data = data_fetcher.fetch_pokemon_species_data(pokemon_id=i)
+
+        # Generates markdown
+        generator = GenerateContent()
+        generator.generate_markdown(file_name=str(i),
+                                    base_data=base_data,
+                                    species_data=species_data)
+
+
+def clear_content():
+    try:
+        dir_path = 'content/pokemon/'
+        shutil.rmtree(dir_path)
+        os.mkdir(dir_path)
+    except Exception as e:
+        raise e
 
 
 def main():
-    data_fetcher = DataFetcher()
-    sample = data_fetcher.fetch_pokemon_data(1)
-    print(sample["name"])
-
-    sample2 = data_fetcher.fetch_pokemon_species_data(1)
-    print(sample2["is_baby"])
+    load_data(NUMBER_OF_POKEMON)
+    # clear_content()
 
 
 if __name__ == '__main__':
