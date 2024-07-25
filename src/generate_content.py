4c405ea0feb54af20089856442ff19b30604b97e
odhyp <pradhana.odhy@gmail.com>
2024-07-25 09:51:26 +0700

    style: add docstrings and type hints

--- src/generate_content.py
+++ src/generate_content.py
@@ -5,7 +5,21 @@ OUTPUT_PATH = "content/pokemon/"
 
 
 class GenerateContent:
-    def get_pokemon_type(self, types_list):
+    """
+    A class to generate markdown content for Pokémon data.
+    """
+
+    def get_pokemon_type(self, types_list: list):
+        """
+        Retrieves the types of a Pokémon from the provided list.
+
+        Args:
+            types_list (list): A list of dictionaries containing type information.
+
+        Returns:
+            list: A list of type names. If there are two types, both are returned. If there is one type, only that type is returned.
+            None: If the list is empty.
+        """
         if len(types_list) == 2:
             return [types_list[0]["type"]["name"], types_list[1]["type"]["name"]]
         elif len(types_list) == 1:
@@ -13,7 +27,18 @@ class GenerateContent:
         else:
             return None
 
-    def generate_markdown(self, file_name, base_data, species_data):
+    def generate_markdown(self,
+                          file_name: str,
+                          base_data: dict,
+                          species_data: dict):
+        """
+        Generates a markdown file for a Pokémon with the provided data.
+
+        Args:
+            file_name (str): The name of the markdown file to be created.
+            base_data (dict): The base data of the Pokémon.
+            species_data (dict): The species data of the Pokémon.
+        """
         output_path = f"{OUTPUT_PATH}{file_name}"
         md = MdUtils(file_name=output_path)
 
