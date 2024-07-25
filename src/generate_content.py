86d29dfe4c2c156b5140b79d6b4b8ffc51fdefea
odhyp <pradhana.odhy@gmail.com>
2024-07-25 09:34:51 +0700

    feat: add getter for pokemon types

--- src/generate_content.py
+++ src/generate_content.py
@@ -5,6 +5,14 @@ OUTPUT_PATH = "content/pokemon/"
 
 
 class GenerateContent:
+    def get_pokemon_type(self, types_list):
+        if len(types_list) == 2:
+            return [types_list[0]["type"]["name"], types_list[1]["type"]["name"]]
+        elif len(types_list) == 1:
+            return [types_list[0]["type"]["name"]]
+        else:
+            return None
+
     def generate_markdown(self, file_name, base_data, species_data):
         output_path = f"{OUTPUT_PATH}{file_name}"
         md = MdUtils(file_name=output_path)
@@ -15,10 +23,9 @@ class GenerateContent:
         md.new_paragraph(f'Name = "{base_data["name"]}"')
         md.new_paragraph(f'Sprites = "{base_data["sprites"]["front_default"]}"')
 
-        type1 = base_data["types"][0]["type"]["name"]
-        type2 = base_data["types"][1]["type"]["name"]
+        types = self.get_pokemon_type(base_data["types"])
 
-        md.new_paragraph(f'Types = ["{type1}", "{type2}"]')
+        md.new_paragraph(f'Types = {types}')
         md.new_paragraph('+++')
         md.new_paragraph('')
 
